5ca45594f31cb8a4823d611e0a08193984049d73
rezwan hossain <rezwanvoot@gmail.com>
2026-03-31 17:29:28 +0600

    feat(auth): implement user authentication check in login and register pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
 import LoginPage from "@/module/login/pages/LoginPage";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // If already logged in, redirect to dashboard/home
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return <LoginPage />;
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
 import RegisterPage from "@/module/register/pages/RegisterPage";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // If already logged in, redirect to dashboard/home
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return <RegisterPage />;
 }
